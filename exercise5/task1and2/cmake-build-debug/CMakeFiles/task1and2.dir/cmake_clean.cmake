00a7982d61d4ed45b4244a96ce9122f2fddc5726
ericyounger <ericy@stud.ntnu.no>
2020-11-16 11:09:52 +0100

    added finished exercises

---
+++ exercise5/task1and2/cmake-build-debug/CMakeFiles/task1and2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task1and2.dir/main.cpp.o"
+  "task1and2"
+  "task1and2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task1and2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
